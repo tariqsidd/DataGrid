fab2c7a675d34e93c7ecde9336d71b88c1d1b40d
Maaz Tariq <mzed046@gmail.com>
2020-11-30 16:19:29 +0500

    asd

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,11 +1,11 @@
 module.exports = {
   baseURL:
     // 'http://192.168.137.16:8000/sales/', // Sherry local
-    'http://18.157.127.204:8000/sales/', // development server
-    // 'http://3.120.229.198/sales/', // QA server
+    // 'http://18.157.127.204:8000/sales/', // development server
+    'http://3.120.229.198/sales/', // QA server
   // 'http://ec2-3-122-192-200.eu-central-1.compute.amazonaws.com/sales/', // production build
   baseImageURL:
-  'http://18.157.127.204:8000/media', // dev
-  // 'http://3.120.229.198/media', // QA
+    // 'http://18.157.127.204:8000/media', // dev
+    'http://3.120.229.198/media', // QA
   // 'http://ec2-3-122-192-200.eu-central-1.compute.amazonaws.com/media', // prod
 };
