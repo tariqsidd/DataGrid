9b03f0f69c728d785e8a58bb11c5c5c5ce756f15
Hamza Murtaza <hamza@dastgyr.com>
2021-01-06 20:46:34 +0500

    updated urls

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,6 @@
 module.exports = {
   baseURL:
-    // 'http://192.168.137.16:8000/sales/', // Sherry local
-    // 'http://18.157.127.204:8000/sales/', // development server
-    // 'http://qa.dastgyr.com/sales/', // QA server
-    'http://api.dastgyr.com/sales/', //Prod
-  // 'http://ec2-3-122-192-200.eu-central-1.compute.amazonaws.com/sales/', // production build
+    // 'http://dev.dastgyr.com/sales/', // development server
+    'http://qa.dastgyr.com/sales/', // QA server
+    // 'http://api.dastgyr.com/sales/', // NEW Prod
 };
