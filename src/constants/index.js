f0a416fd03893f7b70f90e303b3b18d60c990cd8
Rehan Muhammad <rehan@dastgyr.com>
2021-01-27 11:47:09 +0000

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,7 +3,7 @@ module.exports = {
     // 'http://192.168.137.16:8000/sales/', // Sherry local
     // 'http://18.157.127.204:8000/sales/', // development server
     //'http://qa.dastgyr.com/sales/', // QA server
-    'https://api-staging.dastgyr.com/sales', //Staging-Server
+    'https://api-staging.dastgyr.com/sales', //Staging-Env
     // 'http://3.122.192.200/sales/', //Prod
   // 'http://ec2-3-122-192-200.eu-central-1.compute.amazonaws.com/sales/', // production build
 };
