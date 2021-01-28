618990c5c78b132cc5f8b9eddf4cb37f6faaed6b
Wasiq Muhammad <wasiq@dastgyr.com>
2021-01-28 17:58:24 +0500

    test

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,5 +2,5 @@ module.exports = {
   baseURL:
     // 'http://dev.dastgyr.com/sales/', // development server
     // 'http://qa.dastgyr.com/sales/', // QA server
-    'http://api-qa.dastgyr.com/sales/', // NEW Prod
+    'https://api-qa.dastgyr.com/sales/', // NEW Prod
 };
