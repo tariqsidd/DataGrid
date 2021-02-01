d4ef3e602b16d6abfaeaa14a3368dc9d6fb16ccf
Wasiq Muhammad <wasiq@dastgyr.com>
2021-02-01 13:06:39 +0500

    merge sales work

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   baseURL:
-    'http://dev.dastgyr.com/sales/', // development server
-    // 'http://api-qa.dastgyr.com/sales/', // QA server
+    // 'http://dev.dastgyr.com/sales/', // development server
+    'https://api-qa.dastgyr.com/sales/', // QA server
     // 'http://api.dastgyr.com/sales/', // NEW Prod
 };
