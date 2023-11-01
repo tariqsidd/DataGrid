56dd517d681262d511220b1dbf06eab37d8b9f1e
Sumaiyah Chinoy <sumaiyah.chinoy@dastgyr.com>
2023-11-01 15:58:01 +0500

    Added date

--- src/VirtualRender/GenericTextField.js
+++ src/VirtualRender/GenericTextField.js
@@ -1,27 +1,51 @@
-import React,{useRef, useEffect, useState} from 'react';
+import React, { useRef, useEffect, useState } from "react";
+import {
+  KeyboardDatePicker,
+  MuiPickersUtilsProvider,
+} from "@material-ui/pickers";
+import DateFnsUtils from "@date-io/date-fns";
 import { TextField, MenuItem } from "@material-ui/core";
-import {commonStyles} from "../DataGrid/styles";
-import Ajv from 'ajv';
+import { commonStyles } from "../DataGrid/styles";
+import Ajv from "ajv";
 
 const ajv = new Ajv();
-const GenericTextField = ({ type, label, value, onChange, options, schema, validationKey }) => {
+const dateFns = require("date-fns");
+const GenericTextField = ({
+  type,
+  label,
+  value,
+  onChange,
+  options,
+  schema,
+  validationKey,
+}) => {
   const inputRef = useRef(value);
   const [selectedValue, setSelectedValue] = useState(null);
   const [isValid, setIsValid] = useState(true);
   const classes = commonStyles();
 
-  useEffect(()=>{
-    if(type === 'select'){
-      setSelectedValue(value)
+  useEffect(() => {
+    if (type === "select" || type === "date") {
+      setSelectedValue(value);
+    } else {
+      setValue(value);
     }
-    else {
-      setValue(value)
-    }
-  },[]);
+  }, []);
 
   const handleValidation = (val) => {
+    let valueToValidate = {};
+    if (type === "number") {
+      if (val !== undefined) {
+        valueToValidate = { [validationKey]: val };
+      }
+    } else if (type === "text") {
+      if (val.length) {
+        valueToValidate = { [validationKey]: val };
+      }
+    }
     const validate = ajv.compile(schema);
-    const valid = validate({ [validationKey]: val });
+    // const valid = validate({ [validationKey]: val });
+    const valid = validate(valueToValidate);
     setIsValid(valid);
   };
 
@@ -31,7 +55,7 @@ const GenericTextField = ({ type, label, value, onChange, options, schema, valid
     }
   };
 
-  if (type === 'select') {
+  if (type === "select") {
     return (
       <TextField
         select
@@ -42,12 +66,15 @@ const GenericTextField = ({ type, label, value, onChange, options, schema, valid
         inputRef={inputRef}
         value={selectedValue}
         className={classes.textField}
-        onChange={(e)=>{
+        onChange={(e) => {
+          console.log(typeof e.target.value);
           setValue(e.target.value);
           setSelectedValue(e.target.value);
+          // con
           handleValidation(inputRef.current.value);
           onChange(inputRef.current.value, isValid);
-        }}>
+        }}
+      >
         {options.map((option) => (
           <MenuItem key={option.value} value={option.value}>
             {option.label}
@@ -57,18 +84,74 @@ const GenericTextField = ({ type, label, value, onChange, options, schema, valid
     );
   }
 
+  if (type === "date") {
+    return (
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <KeyboardDatePicker
+          className={classes.dateField}
+          margin="dense"
+          error={!isValid}
+          helperText={null}
+          ref={inputRef}
+          variant="inline"
+          format="dd/MM/yyyy"
+          inputVariant="outlined"
+          value={dateFns.parse(selectedValue, "dd/MM/yyyy", new Date())}
+          onChange={(date) => {
+            const dateValid = dateFns.isValid(date);
+            if (dateValid) {
+              const formattedDate = dateFns.format(date, "dd/MM/yyyy");
+              setValue(formattedDate);
+              setSelectedValue(formattedDate);
+              handleValidation(inputRef.current.value);
+
+              //setEditingValue(formattedDate);
+            } else {
+              setValue("");
+              setSelectedValue("");
+              handleValidation(inputRef.current.value);
+              // setEditingValue("");
+            }
+          }}
+          onClose={() => {
+            // onChange(inputRef.current.value, isValid);
+          }}
+          // minDate={
+          //   header.headerDateProperties && header.headerDateProperties.min
+          //     ? dateFns.parse(
+          //         header.headerDateProperties.min,
+          //         "dd/MM/yyyy",
+          //         new Date()
+          //       )
+          //     : undefined
+          // }
+          // maxDate={
+          //   header.headerDateProperties && header.headerDateProperties.max
+          //     ? dateFns.parse(
+          //         header.headerDateProperties.max,
+          //         "dd/MM/yyyy",
+          //         new Date()
+          //       )
+          //     : undefined
+          // }
+          inputProps={{ readOnly: true }}
+        />
+      </MuiPickersUtilsProvider>
+    );
+  }
+
   return (
     <TextField
       autoFocus
       type={type}
       error={!isValid}
       placeholder={label}
-      onChange={(e)=>{
+      onChange={(e) => {
         setValue(e.target.value);
         handleValidation(e.target.value);
       }}
-      onBlur={()=>{
-        onChange(inputRef.current.value, isValid)
+      onBlur={() => {
+        onChange(inputRef.current.value, isValid);
       }}
       margin="dense"
       variant="outlined"
